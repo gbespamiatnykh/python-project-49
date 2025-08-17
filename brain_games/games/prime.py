e17183fe9f4f009d9ed697f731be94458ca05622
George Bespamiatnykh <george.bespamiatnykh@gmail.com>
2025-08-18 01:56:54 +0700

    refactor prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,15 +3,17 @@ from random import randint
 
 from brain_games.constants import MAX_NUMBER, MIN_NUMBER
 
+MIN_PRIME_NUMBER = 2
+
 
 def show_description() -> None:
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
 
 
 def is_prime(number: int) -> bool:
-    if number < 2:
+    if number < MIN_PRIME_NUMBER:
         return False
-    for divider in range(2, int(math.sqrt(number)) + 1):
+    for divider in range(MIN_PRIME_NUMBER, int(math.sqrt(number)) + 1):
         if number % divider == 0:
             return False
     return True
